cca975dce936de476ac04ad102cbd86340c32286
Jay Stotz <jason.stotz@gmail.com>
2015-04-05 22:53:47 -0400

    Add spec_helper.rb and .rspec generated from rspec --init

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+Dir['./spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 10
+
+  config.order = :random
+  Kernel.srand config.seed
+end
