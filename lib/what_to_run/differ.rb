285773fdddb24326cf2a760100ce98338f7febc4
Dyego Costa <dyego@dyegocosta.com>
2015-04-08 02:37:29 -0300

    Predict tests to run when modify uncovered lines
    This introduces false positive results in order to
    include lines evaluated on require, blank lines and
    method and class declarations
    
    Right now the false positives seems inevitable

--- lib/what_to_run/differ.rb
+++ lib/what_to_run/differ.rb
@@ -1,31 +1,65 @@
 module WhatToRun
   class Differ
     class << self
-      def coverage_delta(before, after)
-        after.each_with_object({}) do |(file_name, after_lines_cov), delta|
-          before_lines_cov = before[file_name]
+      ##
+      # Gives the delta beteween the coverage result
+      # before and after a test run and before start
+      # the test suite
+      #
+      # Results in the lines that may trigger the test
+      # that gave the after result
 
-          # skip arrays that are exactly the same
-          next if before_lines_cov == after_lines_cov
+      def coverage_delta(cov_before, cov_after, cov_before_suite)
+        cov_after.each_with_object({}) do |(file_name, lines_cov_after), delta|
+          lines_cov_before = cov_before[file_name]
+          lines_cov_before_suite = cov_before_suite[file_name]
 
-          # subtract the old coverage from the new coverage
-          lines_delta = lines_cov_delta(before_lines_cov, after_lines_cov)
+          next unless file_covered?(lines_cov_before, lines_cov_after)
 
-          # add the "diffed" coverage to the hash
+          lines_delta = lines_cov_delta \
+            lines_cov_before_suite, lines_cov_before, lines_cov_after
+
           delta[file_name] = lines_delta
         end
       end
 
-      def lines_cov_delta(before_lines_cov, after_lines_cov)
-        after = Array(after_lines_cov)
-        before = Array(before_lines_cov)
+      ##
+      # It needs to diff the diff of before and after
+      # with the coverage before the test suite in order
+      # to include the uncovered lines to all tests that
+      # touch this file
+
+      def lines_cov_delta(before_suite, before, after)
+        delta = diff before_suite, diff(before, after)
+        delta.map(&method(:normalize_cov_result))
+      end
+
+      def diff(before, after)
+        after = Array(after)
+        before = Array(before)
 
         after.zip(before).map do |lines_after, lines_before|
           lines_after ? lines_after - lines_before.to_i : lines_after
         end
       end
+
+      ##
+      # Marks lines to be ran
+      #
+      # @param result positive => covered
+      # @param result negative => covered
+      # @param result nil      => not covered
+      # @param result zero     => not covered; within a method
+
+      def normalize_cov_result(result)
+        result.nil? || result.to_i < 0 ? 1 : result
+      end
+
+      def file_covered?(cov_before, cov_after)
+        cov_before != cov_after
+      end
     end
 
-    private_class_method :lines_cov_delta
+    private_class_method :lines_cov_delta, :file_covered?
   end
 end
