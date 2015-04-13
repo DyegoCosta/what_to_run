6991ffb069ba641143afc4f6f1ad3fbcece9ab4f
Dyego Costa <dyego@dyegocosta.com>
2015-04-12 22:05:36 -0300

    Extract additional databases method

--- lib/what_to_run/tracker.rb
+++ lib/what_to_run/tracker.rb
@@ -8,8 +8,10 @@ module WhatToRun
   class Tracker
     FileUtils.mkdir_p('.what_to_run')
 
+    DB_NAME = 'run_log'.freeze
+
     DB = SQLite3::Database.open \
-      ".what_to_run/run_log#{ENV['TEST_ENV_NUMBER']}.db"
+      ".what_to_run/#{DB_NAME}#{ENV['TEST_ENV_NUMBER']}.db"
 
     class << self
       def start
@@ -39,29 +41,34 @@ module WhatToRun
 
       def read
         query = 'select description, log from coverage'
-        databases_count = Dir['.what_to_run/*.db'].length
 
-        if databases_count > 1
-          attach_databases!(databases_count)
-          query += union_query(databases_count)
+        unless additional_databases.empty?
+          attach_databases!
+          query += union_query
         end
 
-        DB.execute(query).map {|row| [row[0], Marshal.load(row[1])]}
+        DB.execute(query).map { |row| [row[0], Marshal.load(row[1])] }
       end
 
-      def attach_databases!(count)
-        (2..count).each do |n|
-          DB.execute "attach '.what_to_run/run_log#{n}.db' as db#{n}"
+      def additional_databases
+        count = Dir['.what_to_run/*.db'].length
+        (2..count).map { |n| "#{DB_NAME}#{n}" }
+      end
+
+      def attach_databases!
+        additional_databases.each do |db|
+          DB.execute "attach '.what_to_run/#{db}.db' as #{db}"
         end
       end
 
-      def union_query(count)
-        (2..count).inject('') do |q, n|
-          q += " union select description, log from db#{n}.coverage"
+      def union_query
+        additional_databases.inject('') do |query, db|
+          query += " union select description, log from #{db}.coverage"
         end
       end
     end
 
-    private_class_method :attach_databases!, :union_query
+    private_class_method :additional_databases,
+      :attach_databases!, :union_query
   end
 end
--- spec/what_to_run/tracker_spec.rb
+++ spec/what_to_run/tracker_spec.rb
@@ -3,22 +3,45 @@ require 'what_to_run/tracker'
 describe WhatToRun::Tracker do
   subject {described_class}
 
-  describe '.attach_databases!' do
-    it 'attaches databases with numbered suffix starting from 2' do
-      expect(subject::DB).to receive(:execute).once
-        .with("attach '.what_to_run/run_log2.db' as db2")
+  describe '.additional_databases' do
+    it 'returns empty array if just one db exists' do
+      allow(Dir).to receive(:[]).and_return ['run_log.db']
+      expect(subject.send(:additional_databases)).to eq([])
+    end
 
-      expect(subject::DB).to receive(:execute).once
-        .with("attach '.what_to_run/run_log3.db' as db3")
+    it 'returns additional databases names if more than one exist' do
+      allow(Dir).to receive(:[]).and_return \
+        ['run_log.db', 'run_log2.db', 'run_log3.db']
 
-      subject.send(:attach_databases!, 3)
+      expect(subject.send(:additional_databases)).to \
+        eq(['run_log2', 'run_log3'])
     end
   end
 
-  describe '.union_query' do
-    it 'concats union query for each additional database' do
-      expect(subject.send(:union_query, 2)).to eq \
-        ' union select description, log from db2.coverage'
+  context 'with additional databases' do
+    before do
+      allow(subject).to receive(:additional_databases)
+        .and_return(['db2', 'db3'])
+    end
+
+    describe '.attach_databases!' do
+      it 'attaches databases with numbered suffix starting from 2' do
+        expect(subject::DB).to receive(:execute).once
+          .with("attach '.what_to_run/db2.db' as db2")
+
+        expect(subject::DB).to receive(:execute).once
+          .with("attach '.what_to_run/db3.db' as db3")
+
+        subject.send(:attach_databases!)
+      end
+    end
+
+    describe '.union_query' do
+      it 'concats union query for each additional database' do
+        expect(subject.send(:union_query)).to eq \
+          ' union select description, log from db2.coverage' \
+          ' union select description, log from db3.coverage' \
+      end
     end
   end
 end
