87742549dae6fdc730c814386f4e28ea00d9aeff
Dyego Costa <dyego@dyegocosta.com>
2015-04-19 15:57:12 -0300

    Add simple sanity tests for what_to_run command

--- spec/sanity/fake_app/test/calculator_test.rb
+++ spec/sanity/fake_app/test/calculator_test.rb
@@ -1,12 +1,13 @@
+require 'what_to_run/minitest'
 require 'minitest/autorun'
 require File.expand_path('../lib/calculator', File.dirname(__FILE__))
 
 class TestCalculator < MiniTest::Test
   def test_sum
-    assert 3, Calculator.sum(1, 2)
+    assert_equal 3, Calculator.sum(1, 2)
   end
 
   def test_subtract
-    assert 1, Calculator.subtract(2, 1)
+    assert_equal 1, Calculator.subtract(2, 1)
   end
 end
--- spec/sanity/sanity_shared_examples.rb
+++ spec/sanity/sanity_shared_examples.rb
@@ -32,6 +32,54 @@ shared_examples 'a sanity check' do |framework, run_cmd|
         run_log_path = File.join(what_to_run_dir, 'run_log.db')
         expect(File.exist?(run_log_path)).to be_truthy
       end
+
+      describe 'what_to_run command' do
+        let(:what_to_run_result) { @what_to_run_result }
+        let(:process_status) { @process_status }
+
+        let(:what_to_run) do
+          -> do
+            reader, writer = IO.pipe
+
+            fork do
+              reader.close
+
+              writer.write `cd #{sandbox_app_path} &&
+                BUNDLE_GEMFILE=./Gemfile bundle exec what_to_run #{framework}`
+            end
+
+            @process_status = Process.wait2[1]
+
+            writer.close
+
+            @what_to_run_result = reader.read
+          end
+        end
+
+        context 'without any changes' do
+          it 'runs nothing' do
+            what_to_run.call
+            expect(process_status.exitstatus).to eq(0)
+            expect(what_to_run_result).to eq('')
+          end
+        end
+
+        context 'after modifying lib files' do
+          before do
+            Dir["#{sandbox_app_path}/lib/*_modified.rb"].each do |modified|
+              FileUtils.cp modified, modified.gsub('_modified', '')
+            end
+          end
+
+          it 'runs the predicted examples' do
+            what_to_run.call
+
+            what_to_run_result_matches.each do |match|
+              expect(what_to_run_result).to include(match)
+            end
+          end
+        end
+      end
     end
   end
 end
